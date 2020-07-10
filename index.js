6e9ca4029967628eb1092aa9127a5c295906b577
ashish <akmishra.cse20@gmail.com>
2020-07-10 09:17:45 +0530

    setup static files access for layouts and pages

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 8002;
+const expressLayouts = require('express-ejs-layouts');
 const db=require('./config/mongoose');
 const cookieParser = require('cookie-parser');
 
 app.use(express.urlencoded());
 app.use(cookieParser());
+
+app.use(express.static(./assets));
+
+app.use(expressLayouts);
+
+//extract style and scripts from subpages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+//set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+//use express router
 app.use('/', require('./routes'));
+
+
 app.listen(port, function(err){ 
     if(err)
     {
