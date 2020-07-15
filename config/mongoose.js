a73217d5413ef8160683413af3592b8848f5463f
ashish <akmishra.cse20@gmail.com>
2020-07-15 20:38:48 +0530

    added authentication using Passport-JS

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,18 +1,47 @@
-//required library
+// //required library
+// const mongoose = require('mongoose');
+
+// //connect to database
+// //  mongoose.connect('mongodb://localhost/codeial_db', {useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost/codeial_development');
+
+// //acquire the connection(to check if it is successful)
+// const db = mongoose.connection;
+
+// //error handling
+// db.on('error', console.error.bind(console, 'error connecting to the DataBase'));
+
+// //up and running
+// db.once('open', function(){
+//     console.log('Successfully connected to the DataBase :: MongoDB');
+// });
+
+// module.exports = db;
+
+
+
+
+
+
+
+//try
+
+
+
+
+
 const mongoose = require('mongoose');
 
-//connect to database
-//  mongoose.connect('mongodb://localhost/codeial_db', {useNewUrlParser: true});
-mongoose.connect('mongodb://localhost/codeial_db');
-//mongodb://localhost:27017/
-//acquire the connection(to check if it is successful)
+mongoose.connect('mongodb://localhost/codeial_development');
+
 const db = mongoose.connection;
 
-//error handling
-db.on('error', console.error.bind(console, 'error connecting to the DataBase'));
-//u there??
-//up and running
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+
 db.once('open', function(){
-    console.log('Successfully connected to the DataBase');
+    console.log('Connected to Database :: MongoDB');
 });
-module.exports=db;
+
+
+module.exports = db;
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,24 @@
-//module.exports.actionName=funcn(req, res)
+// //module.exports.actionName=funcn(req, res)
+
+// module.exports.home = function(req, res){
+//     //return res.end('<h1>Express is up for Codeial !!</h1>');
+//     return res.render('home', {
+//         title: "home"
+//     });
+// }
+
+
+
+
+
+//try
 
 module.exports.home = function(req, res){
-    //return res.end('<h1>Express is up for Codeial !!</h1>');
+    console.log(req.cookies);
+    res.cookie('user_id', 25);
     return res.render('home', {
-        title: "home"
+        title: "Home"
     });
 }
 
+// module.exports.actionName = function(req, res){}
