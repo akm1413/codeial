dabc8d330582a37f1225a4b7a168eb196d53d927
ashish <akmishra.cse20@gmail.com>
2020-07-15 21:17:58 +0530

    added the schema for post

--- index.js
+++ index.js
@@ -82,9 +82,10 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 8001;
+const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
@@ -110,14 +111,11 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-
-
-
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
+//mongo store is used to store the session cookie in the db
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
