987c154eec3c4a1366cea531ebe702a548d1c5d0
ashish <akmishra.cse20@gmail.com>
2020-07-17 15:33:08 +0530

    updating a user's profile

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,39 +1,3 @@
-// const Post = require('../models/post')
-// const Comment = require('../models/comment');
-
-// module.exports.create = function(req, res){
-//     Post.create({
-//         content: req.body.content,
-//         user: req.user._id
-//     }, function(err, post){
-//         if(err)
-//         {
-//             console.log('error in creating a post');
-//             return;
-//         }
-//         return res.redirect('back');
-//     });
-// }
-
-// module.exports.destroy = function(req, res){
-//     Post.findById(req.params.id, function(err, post){
-//         //making sure that only the authorized person(who had posted the post) can delete a post
-//         if(post.user == req.user.id){ //.id means converting the object "id" into string
-//             Post.remove();
-
-//             Comment.deleteMany({post: req.params.id}, function(err){
-//                 return res.redirect('back');
-//             });
-//         }
-//         else{
-//             return res.redirect('back');
-//         }
-//     });
-// }
-
-
-
-
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
